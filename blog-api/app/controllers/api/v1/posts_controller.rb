dcee0c1054c9dc575b805b28987e6247b34e5a8b
tohhiro <tohhiro@gmail.com>
2026-01-11 19:26:42 +0900

    Postコントローラのupdateメソッドにif文を追加

--- blog-api/app/controllers/api/v1/posts_controller.rb
+++ blog-api/app/controllers/api/v1/posts_controller.rb
@@ -20,8 +20,11 @@ class Api::V1::PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    render json: @post
+    if(@post.update(post_params))
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
