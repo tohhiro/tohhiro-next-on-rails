b860994a6d2002a0244370a1b7bc42caa4ef5781
tohhiro <tohhiro@gmail.com>
2026-01-20 14:08:55 +0900

    Storybookをアップグレードし、CSF Nextに対応するようpreviewとmainを修正

--- blog-client/.storybook/preview.ts
+++ blog-client/.storybook/preview.ts
@@ -1,21 +1,19 @@
-import type { Preview } from '@storybook/nextjs-vite'
+import { definePreview } from "@storybook/nextjs-vite";
+import addonA11y from "@storybook/addon-a11y";
+import "../app/globals.css";
 
-const preview: Preview = {
+export default definePreview({
+  addons: [addonA11y()],
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
-};
-
-export default preview;
+});
